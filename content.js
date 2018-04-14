beae248d0fe8a51e03e3b9e73765874ba2f27a68
Jonne Huotari <jonne@huotari.me>
2018-04-14 11:51:30 +0300

    stuff

--- content.js
+++ content.js
@@ -1,24 +1,44 @@
 
 var giphy = "https://api.giphy.com/v1/gifs/random?api_key=Zv7BFWc1GaxQe5tDfr9nO9FEes8AcZKa";
 
+var loading = "https://i.imgur.com/ONBvABV.png";
+
+var giphyArray = [];
+
+function randomImage() {
+  return giphyArray[Math.floor(Math.random()*giphyArray.length)];
+}
+
+function changeImage(img, url) {
+  img.srcset = url + " 100w";
+  img.src = url;
+}
+
 function changeSrc(img) {
 
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", giphy, true);
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState == 4) {
-      // JSON.parse does not evaluate the attacker's scripts.
-      var json = JSON.parse(xhr.responseText);
+  try {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", giphy, true);
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState == 4) {
+        // JSON.parse does not evaluate the attacker's scripts.
+        var json = JSON.parse(xhr.responseText);
+        var url = ""
 
-      if ( json.data ) {
-        img.srcset = json.data.images.source.url + " 100w";
-        img.src = json.data.images.source.url;
+        if ( json.data ) {
+          url = json.data.images.source.url
+          giphyArray.push(url)
+        } else {
+          url = randomImage();
+        }
+
+        changeImage(img, url);
       }
     }
+    xhr.send();
+  } catch (e) {
+    changeImage(img, randomImage());
   }
-  xhr.send();
-
-  img.src =  ""
 }
 
 function bgImages() {
@@ -32,31 +52,44 @@ function bgImages() {
   }
 }
 
-chrome.extension.sendMessage({}, function(response) {
-    var readyStateCheckInterval = setInterval(function() {
-    if (document.readyState === "complete") {
-        clearInterval(readyStateCheckInterval);
+function loopImages(images) {
+  for (var i = 0; i < images.length; i++) {
+    img = images[i]
 
-        var images = document.getElementsByTagName("img");
-        for (var i = 0; i < images.length; i++) {
-          changeSrc(images[i]);
-        }
+    if ( img.classList.contains("meme-ok") ) continue;
 
-        /*
-        var divs = document.getElementsByTagName("div");
-        for (var i = 0; i < divs.length; i++) {
-          var div = divs[i];
-
-          if ( div.style.backgroundImage != "" ) {
-            console.log(div.style.backgroundImage)
-          }
-        }
-        */
-
-        // bgImages();
-
+    if ( img.classList.contains("meme-loading") ) {
+      changeSrc(images[i]);
+      img.classList.add("meme-ok");
+      img.classList.remove("meme-loading");
+    } else {
+      img.classList.add("meme-loading")
+      images[i].src = loading;
     }
-    }, 10);
+  }
+}
+
+chrome.extension.sendMessage({}, function(response) {
+  var readyStateCheckInterval = setInterval(function() {
+
+    /*
+    var divs = document.getElementsByTagName("div");
+    for (var i = 0; i < divs.length; i++) {
+      var div = divs[i];
+
+      if ( div.style.backgroundImage != "" ) {
+        console.log(div.style.backgroundImage)
+      }
+    }
+    */
+
+    // bgImages();
+
+
+    loopImages(document.getElementsByTagName("img"));
+    loopImages(document.getElementsByTagName("video"));
+
+  }, 40);
 });
 
 window.addEventListener ("load", myMain, false);
